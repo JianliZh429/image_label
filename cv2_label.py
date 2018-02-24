cf09974e061248fdb4a50d1eeb006a922681d558
dew.maple <dew.maple@gmail.com>
2018-02-24 15:32:49 +0800

    change color and fix bug of remove latest rect

--- cv2_label.py
+++ cv2_label.py
@@ -23,7 +23,7 @@ def click_and_crop(event, x, y, flags, param):
         im = image.copy()
     elif event == cv2.EVENT_MOUSEMOVE and drawing is True:
         copied = im.copy()
-        cv2.rectangle(copied, ref_pnt[0], (x, y), (0, 0, 255), 1)
+        cv2.rectangle(copied, ref_pnt[0], (x, y), (10, 34, 234), 1)
         image = copied
 
     elif event == cv2.EVENT_LBUTTONUP and drawing is True:
@@ -38,7 +38,7 @@ def click_and_crop(event, x, y, flags, param):
                 'y1': y1,
                 'y2': y2
             })
-            _update_image()
+            _draw_rectangles()
 
 
 def _next():
@@ -60,25 +60,32 @@ def _create_image_json():
 
 
 def _save_json():
-    global rectangles
     if rectangles:
         print("Rects of {}: {}".format(img_file, rectangles))
         _create_image_json()
-        rectangles = []
 
 
-def _update_image():
-    cloned = im.copy()
+def _draw_rectangles():
+    cloned = raw_image.copy()
     for r in rectangles:
-        cv2.rectangle(cloned, (r['x1'], r['y1']), (r['x2'], r['y2']), (0, 0, 255), 2)
+        cv2.rectangle(cloned, (r['x1'], r['y1']), (r['x2'], r['y2']), (5, 191, 30), 2)
     global image
     image = cloned
 
 
 def _save_image():
-    f_name = img_file.split(os.sep)[-1]
-    out = os.path.join(output_dir, '{}.png'.format(f_name))
-    cv2.imwrite(out, image)
+    if len(rectangles) > 0:
+        f_name = img_file.split(os.sep)[-1]
+        f_name = f_name.split(os.extsep)[0]
+        out = os.path.join(output_dir, '{}.png'.format(f_name))
+        cv2.imwrite(out, image)
+
+
+def _save_result():
+    _save_json()
+    _save_image()
+    global rectangles
+    rectangles = []
 
 
 if __name__ == '__main__':
@@ -95,30 +102,31 @@ if __name__ == '__main__':
 
     cv2.namedWindow("Label", cv2.WINDOW_AUTOSIZE)
     cv2.setMouseCallback("Label", click_and_crop)
-    image, img_file = _next()
-
+    raw_image, img_file = _next()
+    image = raw_image.copy()
     while True:
         try:
             cv2.setWindowTitle("Label", img_file)
             cv2.imshow("Label", image)
         except StopIteration as e:
-            _save_json()
-            _save_image()
+            _save_result()
             break
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("n"):
-            _save_json()
-            _save_image()
-            image, img_file = _next()
+            _save_result()
+            raw_image, img_file = _next()
+            image = raw_image.copy()
         # if the 'c' key is pressed, break from the loop
         elif key == 8:
             if rectangles:
+                print("rectangles:{}".format(rectangles))
                 rectangles.pop()
-                _update_image()
+                _draw_rectangles()
+            else:
+                print("No rectangle to remove")
         elif key == ord("c"):
-            _save_json()
-            _save_image()
+            _save_result()
             break
 
     # close all open windows
