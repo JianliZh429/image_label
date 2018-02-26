69585e9b6e8922ec829ee24c8518c30ed9ee28ed
dew.maple <dew.maple@gmail.com>
2018-02-26 10:14:11 +0800

    add resize tool which depends on cv2

---
+++ resize.py
@@ -0,0 +1,36 @@
+import argparse
+
+import cv2
+import os
+
+from utils import images_in_dir, f_name
+
+
+def resize(in_dir, out_dir, dsize):
+    img_files = images_in_dir(in_dir)
+
+    for img in img_files:
+        fname = f_name(img, without_ext=False)
+        image_path = os.path.join(out_dir, fname)
+        im = cv2.imread(img)
+        resized = cv2.resize(im, dsize)
+        cv2.imwrite(image_path, resized)
+        print('{} done.'.format(img))
+        
+    print("All done, {} images.".format(len(img_files)))
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--images_dir", required=True, help="Directories of the images")
+    ap.add_argument("-o", "--output_dir", required=True, help="Directories of output")
+    ap.add_argument("-s", "--scale", required=True, help="scale to resize")
+
+    args = vars(ap.parse_args())
+
+    images_dir = args['images_dir']
+    output_dir = args['output_dir']
+    scale = args['scale']
+    x, y = scale.split(':')
+
+    resize(images_dir, output_dir, (int(x), int(y)))
--- utils.py
+++ utils.py
@@ -10,6 +10,14 @@ def file_in_dir(directory, extends):
     return files
 
 
+def f_name(file_path, without_ext=False):
+    fname = file_path.split(os.sep)[-1]
+    if not without_ext:
+        return fname
+    else:
+        return fname.split(os.extsep)[0]
+
+
 def images_in_dir(image_dir):
     return file_in_dir(image_dir, ('*.png', '*.gif', '*.jpg', '*.jpeg'))
 
